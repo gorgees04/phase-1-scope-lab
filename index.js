7c008ef57b4698f02f37aa9c73a00a662f3eabec
Gorgees Odisho <gorgees041@gmail.com>
2023-09-30 22:20:42 +0300

    DONE WITH SCOPE VARIABLE ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// customerName
+var customerName = "bob"
+
+// upperCaseCustomerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+// setBestCustomer
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+// overwriteBestCustome
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+// try to change constant variable
+const leastFavoriteCustomer = "some value"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "I can change the value!!"
+}
